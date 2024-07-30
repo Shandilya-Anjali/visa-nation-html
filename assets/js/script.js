ad1265709f6329c2968fd848a00890640b8feb5f
Anjali-Shandilya <anjalis.encircle@gmail.com>
2024-07-30 18:58:03 +0530

    footer in-progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,8 @@ $(document).ready(function ($) {
     if ($('.zigzag-slider-wrapper').length) {
         var zigzag_swiper = new Swiper(".zigzag-swiper", {
             slidesPerView: 1,
-            spaceBetween: 40,
+            spaceBetween: 88,
+            slideShadows: false,
             loop: false,
             // effect: "fade",
             speed: 2500,
@@ -32,10 +33,68 @@ $(document).ready(function ($) {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
+            slideShadows: false,
             fadeEffect: {
                 crossFade: true
             },
         });
     }
+
+    // Function to toggle accordion items
+    function toggleAccordion($this) {
+        var $content = $this.siblings(".accordion-content");
+        var $plusIcon = $this.find(".plus");
+        var $minusIcon = $this.find(".minus");
+
+        if ($this.hasClass("active")) {
+            $this.removeClass("active");
+            $content.slideUp(200);
+            $plusIcon.show();
+            $minusIcon.hide();
+        } else {
+            $(".accordion-item > a").removeClass("active").find(".plus").show().siblings(".minus").hide();
+            $(".accordion-content").slideUp(200);
+
+            $this.addClass("active");
+            $content.slideDown(200);
+            $plusIcon.hide();
+            $minusIcon.show();
+        }
+    }
+
+    $(".accordion-item > a").on("click", function (e) {
+        e.preventDefault(); // Prevent the default action of the link
+        toggleAccordion($(this));
+    });
+
+    // Initialize: Hide all minus icons, show first accordion item
+    $(".minus").hide();
+    $(".accordion-content").slideUp(200);
+    var $firstAccordion = $(".accordion-item:first > a");
+    $firstAccordion.addClass("active").siblings(".accordion-content").slideDown(200);
+    $firstAccordion.find(".plus").hide().siblings(".minus").show();
+
+
+    //counter
+    let count = document.querySelectorAll(".counter")
+    let arr = Array.from(count)
+
+    arr.map(function (item) {
+        let startnumber = 0
+
+        function counterup() {
+            startnumber++
+            item.innerHTML = startnumber
+
+            if (startnumber == item.dataset.number) {
+                clearInterval(stop)
+            }
+        }
+
+        let stop = setInterval(function () {
+            counterup()
+        }, 2)
+
+    })
 
 });
