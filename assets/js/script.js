ea285b898b32eab1dba9fa1fb4b24e152064f41f
Anjali-Shandilya <anjalis.encircle@gmail.com>
2024-07-30 14:34:52 +0530

    zigzag-slider inprogress

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,22 @@ $(document).ready(function ($) {
             $('.mega-menu ul').stop().slideUp(100);
         });
 
+    //banner-slider 
+    if ($('.zigzag-slider-wrapper').length) {
+        var zigzag_swiper = new Swiper(".zigzag-swiper", {
+            slidesPerView: 1,
+            spaceBetween: 40,
+            loop: false,
+            // effect: "fade",
+            speed: 2500,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            fadeEffect: {
+                crossFade: true
+            },
+        });
+    }
+
 });
