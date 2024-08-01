ca6548adbe58210893422593cf960ed6f979f130
Anjali-Shandilya <anjalis.encircle@gmail.com>
2024-08-01 12:17:09 +0530

    updated site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,5 @@
 $(document).ready(function ($) {
-     // Responsive menu start
-    //  (function ($) {
-    //     $.fn.menumaker = function (options) {
-    //         var cssmenu = $(this),
-    //             settings = $.extend({
-    //                 format: "dropdown",
-    //                 sticky: false
-    //             }, options);
-    //         return this.each(function () {
-    //             $(this).find(".navbar-toggler").on('click', function () {
-    //                 $(this).toggleClass('menu-opened');
-    //                 var mainmenu = $(this).next('#menu-main-menu');
-    //                 if (mainmenu.hasClass('open')) {
-    //                     mainmenu.slideToggle().removeClass('open');
-    //                 } else {
-    //                     mainmenu.slideToggle().addClass('open');
-    //                     if (settings.format === "dropdown") {
-    //                         mainmenu.find('#menu-main-menu').show();
-    //                     }
-    //                 }
-    //             });
-    //             cssmenu.find('li ul.sub-menu, li ul.sub-inner-menu').parent().addClass('has-sub');
-    //             function multiTg() {
-    //                 cssmenu.find(".has-sub").prepend('<span class="submenu-button"><svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.50495 5.64494L0.20524 1.3451C-0.0684144 1.07157 -0.0684144 0.628106 0.20524 0.354717C0.478651 0.0813054 0.922098 0.0813054 1.19549 0.354717L5.00007 4.15941L8.80452 0.354827C9.07805 0.081416 9.52145 0.081416 9.79486 0.354827C10.0684 0.628238 10.0684 1.07168 9.79486 1.34521L5.49508 5.64505C5.35831 5.78175 5.17925 5.85003 5.00009 5.85003C4.82085 5.85003 4.64165 5.78162 4.50495 5.64494Z"/></svg></span>');
-    //                 cssmenu.find('.submenu-button').on('click', function () {
-    //                     if ($(this).hasClass('submenu-opened')) {
-    //                         cssmenu.find('.submenu-button').removeClass('submenu-opened');
 
-    //                     } else {
-    //                         cssmenu.find('.submenu-button').removeClass('submenu-opened');
-    //                         $(this).addClass('submenu-opened');
-    //                     }
-
-    //                     if ($(this).siblings('ul').hasClass('open')) {
-    //                         cssmenu.find('.submenu-button').siblings('ul').removeClass('open');
-    //                         cssmenu.find('.submenu-button').siblings('a').removeClass('active');
-    //                     } else {
-    //                         cssmenu.find('.submenu-button').siblings('ul').removeClass('open');
-    //                         cssmenu.find('.submenu-button').siblings('a').removeClass('active');
-    //                         $(this).siblings('ul').addClass('open');
-    //                         $(this).siblings('a').addClass('active');
-    //                     }
-    //                 });
-    //                 cssmenu.find('a').on('click', function () {
-    //                     if ($(this).attr('href') == '#') {
-
-    //                         $(this).siblings('span').toggleClass('submenu-opened');
-    //                         if ($(this).siblings('ul').hasClass('open')) {
-    //                             $(this).siblings('ul').removeClass('open');
-    //                         } else {
-    //                             $(this).siblings('ul').addClass('open');
-    //                         }
-    //                     }
-    //                 });
-    //             };
-
-    //             if (settings.format === 'multitoggle') multiTg();
-    //             else cssmenu.addClass('dropdown');
-    //             if (settings.sticky === true) cssmenu.css('position', 'fixed');
-    //             function resizeFix() {
-    //                 var mediasize = 1199.9;
-    //                 if ($(window).width() > mediasize) {
-    //                     cssmenu.find('ul').addClass('open');
-    //                 }
-    //                 if ($(window).width() <= mediasize) {
-    //                     cssmenu.find('ul').removeClass('open');
-    //                 }
-    //             };
-
-                
-    //             resizeFix();
-    //             return $(window).on('resize', resizeFix);
-    //         });
-    //     };
-    // })($);
-// Responsive menu end
     
     // Sticky Header
     $(window).scroll(function () {
@@ -118,10 +43,6 @@ $(document).ready(function ($) {
         }
     });
 
-    // $(".nav-bar").menumaker({
-    //     title: "Menu",
-    //     format: "multitoggle"
-    // });
 
     //banner-slider 
     if ($('.zigzag-slider-wrapper').length) {
